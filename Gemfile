8dbe52c5a079fb99aedbc5008bcbc1c1befc71cb
Vince Banzon <vince@launchlabs.ph>
2015-04-23 11:16:58 +0800

    back to chapter 10 for 'admin' column

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'uglifier',             '2.5.3'
 gem 'coffee-rails',         '4.1.0'
 gem 'jquery-rails',         '4.0.3'
 gem 'turbolinks',           '2.3.0'
+gem 'faker',                '1.1.2'
+gem "will_paginate",      "~> 3.0.4" 
 gem 'jbuilder',             '2.2.3'
 gem 'sdoc',                 '0.4.0', group: :doc
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -15,7 +15,7 @@ class UserTest < ActiveSupport::TestCase
 
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
-                      password: "foobar", password_confirmation: "foobar")
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -72,5 +72,13 @@ class UserTest < ActiveSupport::TestCase
 
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
+  end
+  
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy                                       # destroys every micropost for destroyed user
+    end
   end
 end
